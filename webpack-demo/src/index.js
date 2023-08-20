8753f00f735effb3ec1b2a99cbd253e6260c2642
Thunderblarg <Thunderblarg@gmail.com>
2023-08-19 21:51:24 -0700

    finished webpack tutorials

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash';
-import myName from "./myName";
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.textContent = myName("Josh");
+    const btn = document.createElement('button');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
   }
   
