7bf1601658e9eaa811b880d39afcc69721ca2a3a
Thunderblarg <Thunderblarg@gmail.com>
2023-11-06 00:09:49 -0800

    really mad about a stupid bug with how I was implementing add buttons. Things work now, saving progress before moving on

--- To-do List/src/index.js
+++ To-do List/src/index.js
@@ -1,76 +1,79 @@
-import toDoList from "./toDoList";
+
 import uiController from "./uiController";
 import "./style.css";
 
 document.addEventListener('DOMContentLoaded', function (){
-    let createdDate = new Date();
-    let newDueDate = new Date(createdDate);
+    // let createdDate = new Date();
+    // let newDueDate = new Date(createdDate);
     let controller = uiController();
-    newDueDate.setDate(newDueDate.getDate() + 1);
+    // newDueDate.setDate(newDueDate.getDate() + 1);
 
-    let body = document.getElementsByTagName("body")[0];
-    body.classList.add("body");
+    
     //console.log(body);
 
-    let categoryList = [];
+    
 
-    let thisIsAList = toDoList("Garage stuff");
+    // let thisIsAList = toDoList("Garage stuff");
 
-    thisIsAList.newToDo("get into garage",
-                        "walk your stupid ass into the garage",
-                        createdDate,
-                        newDueDate,
-                        "High");
+    // thisIsAList.newToDo("get into garage",
+    //                     "walk your stupid ass into the garage",
+    //                     createdDate,
+    //                     newDueDate,
+    //                     "High");
 
-    thisIsAList.newToDo("load truck",
-                        "load up the truck with as much shit as possible and run it to the dump",
-                        createdDate,
-                        newDueDate,
-                        "High"
-    );
+    // thisIsAList.newToDo("load truck",
+    //                     "load up the truck with as much shit as possible and run it to the dump",
+    //                     createdDate,
+    //                     newDueDate,
+    //                     "High"
+    // );
 
-    thisIsAList.newToDo("sand chair",
-                        "get alia's chair sanded so she can refinish it",
-                        createdDate,
-                        newDueDate,
-                        "Medium");
+    // thisIsAList.newToDo("sand chair",
+    //                     "get alia's chair sanded so she can refinish it",
+    //                     createdDate,
+    //                     newDueDate,
+    //                     "Medium");
 
-    categoryList.push(thisIsAList);
+    // categoryList.push(thisIsAList);
 
     
-    let anotherList = toDoList("Crafting shit");
+    // let anotherList = toDoList("Crafting shit");
 
-    anotherList.newToDo("work on harness",
-                        "start working on the titty harness for jack",
-                        createdDate,
-                        newDueDate,
-                        "High");
+    // anotherList.newToDo("work on harness",
+    //                     "start working on the titty harness for jack",
+    //                     createdDate,
+    //                     newDueDate,
+    //                     "High");
 
-    anotherList.newToDo("finish staining chair",
-                        "get the stain put on the chair so it's done being blackened",
-                        createdDate,
-                        newDueDate,
-                        "Medium");
+    // anotherList.newToDo("finish staining chair",
+    //                     "get the stain put on the chair so it's done being blackened",
+    //                     createdDate,
+    //                     newDueDate,
+    //                     "Medium");
 
-    anotherList.newToDo("finish viking chairs",
-                        "finish viking chairs so they're ready to sell",
-                        createdDate,
-                        newDueDate,
-                        "Medium");
+    // anotherList.newToDo("finish viking chairs",
+    //                     "finish viking chairs so they're ready to sell",
+    //                     createdDate,
+    //                     newDueDate,
+    //                     "Medium");
 
-    categoryList.push(anotherList);
+    // categoryList.push(anotherList);
 
-    let pageContainer = document.createElement('div');
-    pageContainer.classList.add("pageContainer");
-    body.appendChild(pageContainer);
     
-    let contentContainer = document.createElement('div');
-    contentContainer.classList.add("contentContainer");
-    pageContainer.appendChild(contentContainer);
 
     controller.initializeBoard();
+    // let noteTitles = document.getElementsByClassName("itemTitle"); 
+    // console.log(noteTitles);
 
-    controller.generateToDoCategories(categoryList);
+    // noteTitles.item(0).addEventListener('click', function(event){
+    //     console.log("bar")
+    // });
+    // for (let idx = 0; idx < noteTitles.length; idx++){
+    //     let foo = noteTitles[idx]
+    //     console.log(foo);
+    //     foo.addEventListener("click", function(event){console.log("bar")});
+    // }
+    // controller.generateToDoCategories(categoryList);
 
-    controller.generateToDoItems(categoryList[0]);
+    // controller.generateToDoItems(categoryList[0]);
 });
--- To-do List/src/uiController.js
+++ To-do List/src/uiController.js
@@ -1,19 +1,87 @@
 import "./style.css";
+import toDoList from "./toDoList";
 import checkboxChecked from "./img/checkbox-check.svg";
 import checkboxUnchecked from "./img/checkbox-unchecked.svg";
 import trashCan from "./img/trashcan.svg";
 
 export default function(){
     let parentList;
+    let categoryList = [];
+
+    function loadDefaultData(){
+        let createdDate = new Date();
+        let newDueDate = new Date(createdDate);
+        newDueDate.setDate(newDueDate.getDate() + 1);
+
+        let thisIsAList = toDoList("Garage stuff");
+
+        thisIsAList.newToDo("get into garage",
+                            "walk your stupid ass into the garage",
+                            createdDate,
+                            newDueDate,
+                            "High");
+    
+        thisIsAList.newToDo("load truck",
+                            "load up the truck with as much shit as possible and run it to the dump",
+                            createdDate,
+                            newDueDate,
+                            "High"
+        );
+    
+        thisIsAList.newToDo("sand chair",
+                            "get alia's chair sanded so she can refinish it",
+                            createdDate,
+                            newDueDate,
+                            "Medium");
+    
+        categoryList.push(thisIsAList);
+    
+        
+        let anotherList = toDoList("Crafting shit");
+    
+        anotherList.newToDo("work on harness",
+                            "start working on the titty harness for jack",
+                            createdDate,
+                            newDueDate,
+                            "High");
+    
+        anotherList.newToDo("finish staining chair",
+                            "get the stain put on the chair so it's done being blackened",
+                            createdDate,
+                            newDueDate,
+                            "Medium");
+    
+        anotherList.newToDo("finish viking chairs",
+                            "finish viking chairs so they're ready to sell",
+                            createdDate,
+                            newDueDate,
+                            "Medium");
+    
+        categoryList.push(anotherList);
+    };
 
     function initializeBoard(){
+        loadDefaultData();
+
+        let body = document.getElementsByTagName("body")[0];
+        body.classList.add("body");
+        
+        let pageContainer = document.createElement('div');
+        pageContainer.classList.add("pageContainer");
+        body.appendChild(pageContainer);
+        
+        let contentContainer = document.createElement('div');
+        contentContainer.classList.add("contentContainer");
+        pageContainer.appendChild(contentContainer);
+        
         let container = document.getElementsByClassName("contentContainer")[0];
 
-        let contentControls = document.createElement('div');
-        contentControls.classList.add("contentControls");
-        container.appendChild(contentControls);
+        // i'm so fucking mad about this
+        // let contentControls = document.createElement('div');
+        // contentControls.classList.add("contentControls");
+        // container.appendChild(contentControls);
 
-        generateAddButtons(contentControls);
+        // generateAddButtons(contentControls);
 
         let listContainer = document.createElement('div');
         listContainer.classList.add("toDoPanel");
@@ -26,6 +94,14 @@ export default function(){
         let projectDetails = document.createElement('div');
         projectDetails.classList.add("projectDetails");
         listContainer.appendChild(projectDetails);
+
+        generateToDoCategories();
+
+        generateToDoItems(categoryList[0]);
+
+        attachClickHandlers();
+
+
     }
 
     function generateAddButtons(contentControls){    
@@ -39,9 +115,11 @@ export default function(){
         newToDoItem.innerHTML = "+";
         contentControls.appendChild(newToDoItem);
 
+        attachAddButtonClickHandlers();
+
     }
 
-    function generateToDoCategories(categoryList){
+    function generateToDoCategories(){
         let container = document.getElementsByClassName("projectPanel")[0];
 
         container.innerHTML = "";
@@ -81,13 +159,13 @@ export default function(){
         let title = document.createElement('div');
         title.classList.add('itemTitle');
         title.innerHTML = item.title;
-        attachTitleClickHandlers(title);
         itemContent.appendChild(title);
+        // attachTitleClickHandlers(title);
 
         let details = document.createElement('div');
         details.classList.add('itemDetails');
         details.innerHTML = item.details;
-        attachDetailsClickHandlers(details);
+        // attachDetailsClickHandlers(details);
         itemContent.appendChild(details);
 
         newItem.appendChild(itemContent);
@@ -99,19 +177,19 @@ export default function(){
         
         let checkbox = new Image();
         checkbox.src = checkboxUnchecked;
-        attachCheckboxClickHandlers(checkbox);
+        // attachCheckboxClickHandlers(checkbox);
         controls.appendChild(checkbox);
 
         let deleteButton = new Image();
         deleteButton.src = trashCan;
-        attachDeleteClickHandlers(deleteButton);
+        // attachDeleteClickHandlers(deleteButton);
         controls.appendChild(deleteButton);
 
         itemControls.appendChild(checkbox);
         itemControls.appendChild(deleteButton);
 
 
-        newItem.appendChild(itemControls);
+        newItem.appendChild(itemControls);        
         
         // container.appendChild(newItem);
         return newItem;
@@ -127,46 +205,81 @@ export default function(){
             container.appendChild(generateSingleToDoItem(item));        
         });
     }
+
+    function attachClickHandlers(){
+        let noteTitles = document.getElementsByClassName("itemTitle"); 
+
+        for (let idx = 0; idx < noteTitles.length; idx++){
+            let foo = noteTitles[idx];
+            attachTitleClickHandlers(foo);
+        }
+    }
+        // for (let idx = 0; idx < noteTitles.length; idx++){
+        //     console.log(noteTitles[idx]);
+        //     // attachTitleClickHandlers(noteTitles[idx]);
+        // }
+        
+        // noteTitles.forEach((title) => {
+        //     console.log(title);
+        //     attachTitleClickHandlers(title);
+        // });
+
+
+        
+    
     
     function attachTitleClickHandlers(element){
-        element.addEventListener("click", function(event){
-            event.stopPropagation();                   
+        // let noteTitles = document.getElementsByClassName("itemTitle"); 
 
-            let itemIndex = getElementIndex(this);
+        // for (let idx = 0; idx < noteTitles.length; idx++){
+        //     let foo = noteTitles[idx];
+        //     attachTitleClickHandlers(foo);
+        // }
 
+        // console.log(element);
+        // console.log(noteTitles);
+        // let idx;
+        // for (idx = 0; idx < noteTitles.length; idx++){
+            // console.log(idx);
+            // let element = noteTitles[idx];
+            element.addEventListener("click", function(event){
+                //event.stopPropagation();     
+                console.log("inner event");
 
-            if(!(parentList.getToDo(itemIndex).completed)){
-                let itemText = element.innerHTML;
-                // element.innerHTML = "";
-                let parent = element.parentElement;
-                
+                let itemIndex = getElementIndex(element);
+                if(!(parentList.getToDo(itemIndex).completed)){
+                    let itemText = element.innerHTML;
+                    // element.innerHTML = "";
+                    let parent = element.parentElement;
+                    
 
-                let textField = document.createElement('input');
-                textField.classList.add("itemTitle--editing");
-                textField.type = "text";
-                textField.value = itemText;            
-                
-                parent.removeChild(this);
-                parent.prepend(textField);
-                textField.focus();
+                    let textField = document.createElement('input');
+                    textField.classList.add("itemTitle--editing");
+                    textField.type = "text";
+                    textField.value = itemText;            
+                    
+                    parent.removeChild(this);
+                    parent.prepend(textField);
+                    textField.focus();
 
-                textField.addEventListener("blur", function(event){
-                    event.stopPropagation();
+                    textField.addEventListener("blur", function(event){
+                        event.stopPropagation();
 
-                    let newTitle = this.value;
-                    parentList.updateTitle(itemIndex, newTitle);
+                        let newTitle = this.value;
+                        parentList.updateTitle(itemIndex, newTitle);
 
-                    let title = document.createElement('div');
-                    title.classList.add('itemTitle');
-                    title.innerHTML = parentList.getToDo(itemIndex).title;
-                    attachTitleClickHandlers(title);
+                        let title = document.createElement('div');
+                        title.classList.add('itemTitle');
+                        title.innerHTML = parentList.getToDo(itemIndex).title;
+                        attachTitleClickHandlers(title);
 
-                    parent.removeChild(parent.children[0]);
+                        parent.removeChild(parent.children[0]);
 
-                    parent.prepend(title);
-                });
-            }
-        });
+                        parent.prepend(title);
+                    });
+                }
+            });
+        // }
     }
 
     function attachDetailsClickHandlers(element){
@@ -255,12 +368,20 @@ export default function(){
 
     function getElementIndex(element){
         let parent = element.parentElement;
-
+        console.log(parent.parentElement);
         let labelContainer = parent.parentElement;
         let siblingList = Array.from(labelContainer.parentElement.children);
         let itemIndex = siblingList.indexOf(labelContainer);
-
+        console.log(itemIndex);
+        
         return itemIndex;
+    }
+
+    function attachAddButtonClickHandlers(){
+        let buttons = document.getElementsByClassName("addButton");
+        buttons[0].addEventListener('click', function(){
+
+        });
     }
 
     return {
