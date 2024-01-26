a97f5403dbd11ca8c25a198f6c91f33061d101ae
Thunderblarg <Thunderblarg@gmail.com>
2024-01-25 19:09:41 -0800

    Starting this up

---
+++ menu-module/dist/main.js
@@ -0,0 +1,19 @@
+(self["webpackChunkmenu_module"] = self["webpackChunkmenu_module"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+console.log("fuck this");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbWVudS1tb2R1bGUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJmdWNrIHRoaXNcIik7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
